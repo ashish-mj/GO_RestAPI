849fbb5795687898234b4cda755369d2ea3d0a44
Ashish M J <ashm.jagadeesh@gmail.com>
2021-10-23 01:25:00 +0530

    Create Route functions get books n get book

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -8,10 +10,10 @@ import (
 )
 
 type Book struct {
-	ID     string  `json:"id"`
-	Isbn   string  `json:"isbn"`
-	Title  string  `json:"id"`
-	Author *Author `json:"author"`
+	ID     string `json:"id"`
+	Isbn   string `json:"isbn"`
+	Title  string `json:"title"`
+	Author Author `json:"auth"`
 }
 
 type Author struct {
@@ -19,14 +21,52 @@ type Author struct {
 	Lastname  string `json:"lastname"`
 }
 
+var books []Book
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	fmt.Println(params)
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 	//Init Router
 	r := mux.NewRouter()
+
+	books = append(books, Book{"1", "1087", "3 Point Something", Author{"Steven", "Jose"}})
+	books = append(books, Book{"2", "2087", "Good Life", Author{"Franck", "Jose"}})
+	books = append(books, Book{"3", "3087", "Never Give Up", Author{"John", "hamson"}})
+	books = append(books, Book{"4", "4087", "God", Author{"Roman", "C"}})
 
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
 	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
 	r.HandleFunc("/api/books", createBook).Methods("POST")
 	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
 	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
-	log.Fatal(http.ListenAndServe(":6000", r))
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
