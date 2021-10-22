8c6afb44f7253fcd5515b8a38b766a74ce8a527c
Ashish M J <ashm.jagadeesh@gmail.com>
2021-10-22 12:54:21 +0530

    Create structures

--- main.go
+++ main.go
@@ -3,21 +3,30 @@ package main
 import (
 	"log"
 	"net/http"
-	"math/rand"
-	"strconv"
+
 	"github.com/gorilla/mux"
-	"fmt"
 )
 
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"id"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
 
 func main() {
 	//Init Router
-	r := mux.NewRouter();
+	r := mux.NewRouter()
 
-	r.HandleFunc("/api/books",getBooks).Methods("GET")
-	r.HandleFunc("/api/books/{id}",getBook).Methods("GET")
-	r.HandleFunc("/api/books",createBook).Methods("POST")
-	r.HandleFunc("/api/books/{id}",updateBook).Methods("PUT")
-	r.HandleFunc("/api/books/{id}",deleteBook).Methods("DELETE")
-	log.Fatal(http.ListenAndServe(":6000",r))
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":6000", r))
 }
