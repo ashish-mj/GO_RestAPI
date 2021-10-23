f52534f3969d8f04cbacf688ebae1a955aa11c67
Ashish M J <ashm.jagadeesh@gmail.com>
2021-10-24 00:24:30 +0530

    Add Delete Route method

--- main.go
+++ main.go
@@ -58,7 +58,16 @@ func updateBook(w http.ResponseWriter, r *http.Request) {
 }
 
 func deleteBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	fmt.Println(params)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 func main() {
